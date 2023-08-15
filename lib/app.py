5dea024eb606049d19622dc574b6ab360da5a52b
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-15 14:59:20 +0300

    Completed assignment

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+print('Hello world!')
+print("Hello sun!")
+print("Hello sky!")
+
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+def print_test():
+    print("Hello World! Pass this test, please.\n")
+
+print(print_test())
